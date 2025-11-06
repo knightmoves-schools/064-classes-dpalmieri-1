3b8fdaf158e1b0fe3dadcb3b103850d83211bbe7
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-11-06 21:25:50 +0000

    Better Guest List

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ class Person{
     }
 }
 
-let person1 = new Person('Jacob','Michigan');
-let person2 = new Person('Toyota','Detroit');
-let person3 = new Person('Jklas','Person');
+let guests = [
+    new Person('Jacob','Michigan'),
+    new Person('Toyota','Detroit'),
+    new Person('Jklas','Person'),
+];
 
-function guests(persons){
-    return `Guest List -  ${person1.firstName} ${person1.lastName}, ${person2.firstName} ${person2.lastName}, ${person3.firstName} ${person3.lastName}`;
-}
-
-document.getElementById('result').innerHTML = guests();
+document.getElementById('result').innerHTML = guests;
